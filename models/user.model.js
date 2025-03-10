6a7ce1afb65312dc28659a20d2fe7b3eebd72c8e
rissshaaav-dev <191943910+rissshaaav-dev@users.noreply.github.com>
2025-03-10 19:33:25 +0530

    Add username and email fields to user model with validation

--- models/user.model.js
+++ models/user.model.js
@@ -2,37 +2,39 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      max: 30,
+    {
+        name: {
+            type: String,
+            required: true,
+            max: 30,
+        },
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            max: 20,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
     },
-    username: {
-      type: String,
-      required: true,
-      max: 20,
-    },
-    email: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 userSchema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
+    return await bcrypt.compare(password, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
